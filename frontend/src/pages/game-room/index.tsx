4747871dc91613ddabf064ef48540e318b9e20df
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2025-05-04 23:45:06 +0530

    improved the room page

--- frontend/src/pages/game-room/index.tsx
+++ frontend/src/pages/game-room/index.tsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router"
+import Loader from "../../components/Loader"
+// TODO: HTTP call to get the players in the room
 export default function index() {
-    const params = useParams()
+  const params = useParams()
   const room = params?.room
-  return (
-    <div className="text-black">index:{room}</div>
-  )
+  return <Loader text={`Inside the room ${room}`}/>
 }
